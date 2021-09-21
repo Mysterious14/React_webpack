2f2bbb22dc56aa84851bd53ed618898ba77f32f8
Юрій Ірза <irzaurij04@gmail.com>
2021-09-21 11:41:43 +0300

    first commit

---
+++ bin/dev.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack')
+const [webpackClientConfig, webpackServerConfig] = require('../webpack.config')
+const nodemon = require('nodemon')
+const path = require('path')
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotDevMiddleware = require('webpack-hot-middleware');
+const express = require('express');
+const hmrServer = express();
+
+const clientCompiler = webpack(webpackClientConfig);
+
+hmrServer.use(webpackDevMiddleware(clientCompiler, {
+  publicPath: webpackClientConfig.output.publicPath,
+  serverSideRender: true,
+  noInfo: true,
+  watchOptions: {
+    ignore: /dist/,
+  },
+  writeToDisk: true,
+  stats:'errors-only'
+}));
+
+hmrServer.use(webpackHotMiddleware(clientCompiler,{
+  path: '/static/__webpack_hmr',
+}));
+
+hmrServer.listen(3001, () => {
+  console.log('HMR server succesful started ')
+})
+const compiler = webpack(webpackServerConfig)
+
+compiler.run((err)=>{
+if(err){
+  console.log(`compilation failed:`, err)
+}
+compiler.watch({}, (err)=>{
+  if(err){
+    console.log(`compilation failed:`, err)
+  }
+  console.log('Compilation was successfully')
+});
+
+nodemon({
+  script: path.resolve(__dirname, '../dist/server/server.js'),
+  watch: [
+    path.resolve(__dirname, '../dist/server'),
+    path.resolve(__dirname, '../dist/client')
+]
+})
+})
